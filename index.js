f00d4f0f4d29c0af9bd90c02773fd8217cb7798f
Moisés Suárez <54259994+Moisz22@users.noreply.github.com>
2021-02-24 23:20:51 -0500

    subiendo archivos

---
+++ index.js
@@ -0,0 +1,76 @@
+let tabla = document.getElementById("tabla")
+let busqueda = document.getElementById("boton_busqueda")
+let search = document.getElementById("search")
+let url = 'http://192.168.100.66/apiFrameworks/'
+
+
+busqueda.addEventListener('click', function(e){
+
+    let search = document.getElementById("search")
+
+    console.log(search.value)
+
+    e.preventDefault()
+
+    if(search.value === null || search.value === ''){
+
+        url = 'http://192.168.100.66/apiFrameworks/'
+        
+    }else{
+
+        url = 'http://192.168.100.66/apiFrameworks/?id='+search.value
+
+    }
+
+    console.log(url)
+    console.log(search.value)
+
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+
+        if(json.length == undefined){
+
+            tabla.innerHTML = '<tr><th>ID</th><th>NOMBRE</th><th>LANZAMIENTO</th><th>DESARROLLADOR</th></tr>';
+
+            let elemento = document.createElement('tr')
+            elemento.innerHTML += '<td>'+ json.id + '</td>'
+            elemento.innerHTML += '<td>' + json.nombre + '</td>'
+            elemento.innerHTML += '<td>' + json.lanzamiento + '</td>'
+            elemento.innerHTML += '<td>' + json.desarrollador + '</td>'
+            tabla.appendChild(elemento)
+            url = null
+
+        }else{
+
+            tabla.innerHTML = '<tr><th>ID</th><th>NOMBRE</th><th>LANZAMIENTO</th><th>DESARROLLADOR</th></tr>';
+
+            for(i=0; i< json.length ; i++){
+                let elemento = document.createElement('tr')
+                elemento.innerHTML += '<td>'+ json[i].id + '</td>'
+                elemento.innerHTML += '<td>' + json[i].nombre + '</td>'
+                elemento.innerHTML += '<td>' + json[i].lanzamiento + '</td>'
+                elemento.innerHTML += '<td>' + json[i].desarrollador + '</td>'
+                tabla.appendChild(elemento)
+                url = null
+            }
+
+        }
+
+      })
+
+})
+
+
+
+      
+
+
+
+
+  
+  
+
+
+
+
